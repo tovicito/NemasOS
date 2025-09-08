864d25c8502836949c96cd397f50440756175d5d
tovicito <tovictakamine@gmail.com>
2025-09-08 08:31:46 +0200

    Estoy intentando arreglar bugs

--- tpt_project/handlers/script_handler.py
+++ tpt_project/handlers/script_handler.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from .base_handler import BaseHandler
-from ..utils.logger import Logger
 from ..core.config import Configuracion
 from ..utils.system import execute_command
 from ..utils.exceptions import TPTError
